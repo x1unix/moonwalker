cd272b341a12e126e1883936153a45a949fa5bf8
odin3 <ascii@live.ru>
2016-12-10 03:11:25 +0200

    Search frame url in player_api js

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ require('./vendor/autoload.php');
 use x1unix\Moonwalker;
 session_start();
 
-$r = Moonwalker\Grabber::getPlayerScriptByKinopoiskId(770);
-print_r($r);
+// $r = Moonwalker\Grabber::getPlayerScriptByKinopoiskId(770);
+$r = file_get_contents('./src/x1unix/Moonwalker/data/player_api');
+$results = array();
+preg_match('~(http://moonwalk.co/api/iframe/?)([A-Za-z0-9=&_\-\?\s\.]+)~', $r, $results);
+
+if (count($results) > 0) var_dump($results[0]);
 ?>
---
+++ src/x1unix/Moonwalker/Parser.php
@@ -0,0 +1,10 @@
+<?php
+namespace x1unix\Moonwalker;
+
+
+class Parser
+{
+    public static function getPlayerFrameSrcFromScript($script) {
+        return $script;
+    }
+}
